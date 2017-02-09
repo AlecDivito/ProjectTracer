5fc839cbd9d6dca7c8a40f8be24422a6fc0367bd
Alec Divito <divito.alec@gmail.com>
2017-02-09 22:17:30 +0000

    Updated web.php to route everything through the controller

--- app/Http/Controllers/BasicProjectController.php
+++ app/Http/Controllers/BasicProjectController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class BasicProjectController extends Controller
 {
-    //
+    public function project(Request $request)
+    {
+        return view('projectTracker.project');
+    }
+
+    public function saveProject(Request $request)
+    {
+        return redirect('/project');
+    }
+
+
+    public function contact(Request $request)
+    {
+        return view('projectTracker.contact');
+    }
+
+    public function saveContact(Request $request)
+    {
+        return redirect('/contact');
+    }
 }
