a161929f92c290c4d8a437d5b16e985e47c2f0b0
Alec Divito <divito.alec@gmail.com>
2017-02-09 16:21:23 +0000

    Added basic html for the contact and project page

---
+++ resources/views/projectTracker/contact.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Project Page</title>
+  <link rel="stylesheet" href="../css/app.css">
+</head>
+<body>
+
+<div class="content container">
+  <form method="post" >
+    <div class="buttons">
+      <div class="pull-left">
+        <input type="submit" name="back2" value="<|">
+        <input type="submit" name="back1" value="<">
+        <input type="submit" name="forward1" value=">">
+        <input type="submit" name="forward2" value="|>">
+      </div>
+      <div class="pull-right">
+        <input type="submit" name="new" value="Add New">
+        <input type="submit" name="save" value="Save Changes">
+        <input type="submit" name="delete" value="Delete">
+      </div>
+      <div class="clearfix"></div>
+    </div>
+    <p>Record # of #</p>
+
+    <ul>
+      <li>
+        <label for="">Last Name:<input type="text" name="" id="" required></label>
+        <label for="">First Name:<input type="text" name="" id="" required></label>
+        <label for="">Middle Name:<input type="text" name="" id="" ></label>
+      </li>
+      <li>
+        <label for="">Company:<input type="text" name="" id="" required></label>
+      </li>
+      <li>
+        <label for="">Address 1:<input type="text" name="" id="" required></label>
+      </li>
+      <li>
+        <label for="">Address 2:<input type="text" name="" id=""></label>
+      </li>
+      <li>
+        <label for="">City:<input type="text" name="" id="" required></label>
+        <label for="">State / Region:<input type="text" name="" id="" required></label>
+        <label for="">Postal Code:<input type="text" name="" id="" required></label>
+      </li>
+      <li>
+        <label for="">Work Phone:<input type="text" name="" id="" ></label>
+      </li>
+      <li>
+        <label for="">Home Phone:<input type="text" name="" id="" ></label>
+      </li>
+      <li>
+        <label for="">Cell Phone:<input type="text" name="" id="" ></label>
+      </li>
+      <li>
+        <label for="">Email:<input type="text" name="" id="" required></label>
+      </li>
+    </ul>
+
+    <input type="submit" value="Add Current Contact to Project">
+
+  </form>
+</div>
+
+
+<script src="../js/app.js"></script>
+</body>
+</html>
---
+++ resources/views/projectTracker/project.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Project Page</title>
+  <link rel="stylesheet" href="../css/app.css">
+</head>
+<body>
+
+<div class="content container">
+  <form method="post" >
+    <div class="buttons">
+      <div class="pull-left">
+        <input type="submit" name="back2" value="<|">
+        <input type="submit" name="back1" value="<">
+        <input type="submit" name="forward1" value=">">
+        <input type="submit" name="forward2" value="|>">
+      </div>
+      <div class="pull-right">
+        <input type="submit" name="new" value="Add New">
+        <input type="submit" name="save" value="Save Changes">
+        <input type="submit" name="delete" value="Delete">
+      </div>
+      <div class="clearfix"></div>
+    </div>
+
+    <p>Record # of #</p>
+
+    <div class="inputs">
+      <ul class="pull-left">
+        <li><label for="projId">Project ID:<input type="text" name="projId" id="projId"></label></li>
+        <li><label for="projTitle">Project Title:<input type="text" name="projTitle" id="projTitle"></label></li>
+        <li><label for="">Project Description:<textarea></textarea></label></li>
+        <li><label for="priority">Priority<input type="text" name="priority" id="priority"></label></li>
+      </ul>
+      <ul class="pull-right">
+        <li><label for="referenceNum">Reference/Matter #:<input type="text" name="referenceNum" id="referenceNum"></label></li>
+        <li><label for="budget">Money Budget:<input type="text" name="budget" id="budget"></label></li>
+        <li><label for="budgetLeft">Money to Date:<input type="text" name="budgetLeft" id="budgetLeft"></label></li>
+        <li><label for="budgetHours">Hours Budget:<input type="text" name="budgetHours" id="budgetHours"></label></li>
+        <li><label for="hoursLeft">Hours to Date:<input type="text" name="hoursLeft" id="hoursLeft"></label></li>
+        <li><label for="dueDate">Date Due:<input type="text" name="dueDate" id="dueDate"></label></li>
+        <li><label for="status">Status:<input type="text" name="status" id="status"></label></li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+
+    <div class="comments">
+      <div id="tabs">
+
+        <header>Comments/Tasks</header>
+        <div id="tasks">
+          <label for="commentList">Comments / Tasks<textarea name="commentList" id="commentList" cols="30" rows="10"></textarea></label>
+          <input type="button" name="delComment" value="Delete Comment">
+          <input type="button" name="addComment" value="Add Comment">
+          <label for="comment">Comment / Taks Text:<input type="text" id="comment" name="comment"></label>
+        </div>
+
+        <header>Contacts</header>
+        <div id="contacts">
+          <label for="list">Related Contacts:<textarea name="list" id="list" cols="30" rows="10"></textarea></label>
+          <input type="button" value="Email Selected Contact" name="">
+          <input type="button" value="View Details for Selected Contact" name="">
+          <input type="button" value="Add / Manage Contacts" name="">
+          <input type="button" value="Delete Selected Contact" name="">
+        </div>
+
+        <header>File Attachments</header>
+        <div id="files">
+          <label for="list55">Related File Attachments:<textarea name="list55" id="list55" cols="30" rows="10"></textarea></label>
+          <input type="button" name="" value="Open Selected File">
+          <input type="button" name="" value="Remove File Attachment">
+          <input type="button" name="" value="Add File Attachment">
+          <label for="57">File Description:<input type="text" name="" value=""></label>
+          <label for="62">File Name:<input type="text" name="" value=""></label>
+          <input type="button" value="File Browse">
+        </div>
+      </div>
+    </div>
+  </form>
+
+  <div class="pull-right">
+    <input type="button" value="Show Unclosed">
+    <input type="button" value="Show All">
+  </div>
+
+</div>
+
+<script src="../js/app.js"></script>
+</body>
+</html>
