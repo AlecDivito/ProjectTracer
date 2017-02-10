181d7fed92a669514291c42e5e6d4a26b83269bc
Alec Divito <divito.alec@gmail.com>
2017-02-10 02:13:01 +0000

    Added Nullable objects to be able to quickly create projects

--- database/migrations/2017_02_08_184506_add_Projects_table.php
+++ database/migrations/2017_02_08_184506_add_Projects_table.php
@@ -16,16 +16,16 @@ class AddProjectsTable extends Migration
         Schema::create('Projects', function (Blueprint $table) {
             $table->increments('projectId');        // UniqueID for the project
             $table->integer('userId')->unsigned();  // References the user table
-            $table->string('projectTitle');         // Field Size 25
-            $table->string('projectDescription');   // Field Size 250
-            $table->string('priority');             // Field Size 15
-            $table->string('referenceNum');         // Field Size 25
-            $table->decimal('moneyBudget', 11, 2);
-            $table->decimal('moneyToDate', 11, 2);
-            $table->integer('hoursBudget');          // Field Size Integer
-            $table->integer('hoursToDate');          // Field Size Integer
-            $table->dateTime('dateDue');
-            $table->string('Status');               // Field Size 15
+            $table->string('projectTitle')->nullable();         // Field Size 25
+            $table->string('projectDescription')->nullable();   // Field Size 250
+            $table->string('priority')->nullable();             // Field Size 15
+            $table->string('referenceNum')->nullable();         // Field Size 25
+            $table->decimal('moneyBudget', 11, 2)->nullable();
+            $table->decimal('moneyToDate', 11, 2)->nullable();
+            $table->integer('hoursBudget')->nullable();          // Field Size Integer
+            $table->integer('hoursToDate')->nullable();          // Field Size Integer
+            $table->dateTime('dateDue')->nullable();
+            $table->string('Status')->nullable();               // Field Size 15
             $table->foreign('userId')->references('id')->on('users');
         });
     }
--- database/migrations/2017_02_08_184526_add_Contacts_table.php
+++ database/migrations/2017_02_08_184526_add_Contacts_table.php
@@ -15,19 +15,19 @@ class AddContactsTable extends Migration
     {
         Schema::create('Contacts', function (Blueprint $table) {
             $table->increments('contactId');    // Unique ID for contacts
-            $table->String('lastName');         // Field Size 50
-            $table->String('firstName');        // Field Size 50
-            $table->String('middleName');       // Field Size 50
-            $table->String('company');          // Field Size 50
-            $table->String('address1');         // Field Size 100
-            $table->String('address2');         // Field Size 100
-            $table->String('city');             // Field Size 50
-            $table->String('region');           // Field Size 50
-            $table->String('postalCode');       // Field Size 25
-            $table->String('workPhone');        // Field Size 12
-            $table->String('homePhone');        // Field Size 12
-            $table->String('cellPhone');        // Field Size 12
-            $table->String('email');            // Field Size 50
+            $table->String('lastName')->nullable();         // Field Size 50
+            $table->String('firstName')->nullable();        // Field Size 50
+            $table->String('middleName')->nullable();       // Field Size 50
+            $table->String('company')->nullable();          // Field Size 50
+            $table->String('address1')->nullable();         // Field Size 100
+            $table->String('address2')->nullable();         // Field Size 100
+            $table->String('city')->nullable();             //Field Size 50
+            $table->String('region')->nullable();           // Field Size 50
+            $table->String('postalCode')->nullable();       // Field Size 25
+            $table->String('workPhone')->nullable();        // Field Size 12
+            $table->String('homePhone')->nullable();        // Field Size 12
+            $table->String('cellPhone')->nullable();        // Field Size 12
+            $table->String('email')->nullable();            // Field Size 50
         });
     }
 
