0301fc811b12c9331b05e693917489d35ef3951c
Alec Divito <divito.alec@gmail.com>
2017-02-13 19:16:04 +0000

    Added routes and web controller to handle new routes

--- app/Http/Controllers/BasicFileController.php
+++ app/Http/Controllers/BasicFileController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class BasicFileController extends Controller
 {
-    //
+
+  public function add(Request $request)
+  {
+
+  }
+
+  public function delete(Request $request)
+  {
+
+  }
+
+  public function download(Request $request)
+  {
+
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::delete('/contact/{contact}', 'BasicContactController@deleteContact');
 Route::post('/comment/add', 'BasicCommentController@add');
 Route::delete('/comment/delete', 'BasicCommentController@delete');
 
+Route::post('/file/add',     'BasicFileController@add');
+Route::delete('/file/delete','BasicFileController@delete');
+Route::get('file/download',  'BasicFileController@download');
+
 // Added authentication to the user
 Auth::routes();
 Route::get('/home', 'HomeController@index');
